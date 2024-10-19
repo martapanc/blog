82dbee0e005cd620607c8afb8ba456b759827946
martapanc <marta.panc@gmail.com>
2024-10-19 18:25:42 +0200

    Update gitignore

--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/posts/post-1.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../../chunks/post-1_DUBnJ_f5.mjs';
-export { renderers } from '../../renderers.mjs';
-
-const page = () => _page;
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/posts/post-3.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../../chunks/post-3_up3QnuKf.mjs';
-export { renderers } from '../../renderers.mjs';
-
-const page = () => _page;
-
-export { page };
