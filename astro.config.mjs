e4416665dc9e0ecd213ff384a680275edd053778
martapanc <marta.panc@gmail.com>
2024-10-20 18:21:33 +0200

    Install sitemap and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import react from '@astrojs/react';
-
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog-updates.vercel.app/",
-  integrations: [react()],
-  output: 'server',
-  adapter: vercel()
+    site: "https://blog-updates.vercel.app/",
+    integrations: [react(), sitemap(), tailwind()],
+    output: 'server',
+    adapter: vercel()
 });
